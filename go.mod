706d20a3f806a60524b19f16667aa3f32bc679d1
David Scott <dave.scott@docker.com>
2019-10-03 10:53:38 +0100

    Revert "Convert Windows paths into Linux ones for the VM"
    This reverts commit be9698c516b195bdf28e52a3f527528d9a1f4fd6.

--- go.mod
+++ go.mod
@@ -23,7 +23,6 @@ require (
 	github.com/shibukawa/extstat v0.0.0-20150809151201-4113c04d0977
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3
-	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20190418161225-b43e412143f9 // indirect
 	golang.org/x/net v0.0.0-20190415214537-1da14a5a36f2 // indirect
 	golang.org/x/sys v0.0.0-20190418153312-f0ce4c0180be
--- pkg/synchronization/protocols/desktop/protocol.go
+++ pkg/synchronization/protocols/desktop/protocol.go
@@ -3,19 +3,13 @@ package desktop
 import (
 	"encoding/binary"
 	"encoding/hex"
-	"os"
-	"path"
 	"path/filepath"
-	"strings"
-	"unicode"
 
 	"github.com/mutagen-io/mutagen/pkg/logging"
 	"github.com/mutagen-io/mutagen/pkg/synchronization"
 	"github.com/mutagen-io/mutagen/pkg/synchronization/endpoint/remote"
 	urlpkg "github.com/mutagen-io/mutagen/pkg/url"
 )
-
-const mutagenDirInVM = "/mutagen"
 
 // protocolHandler implements the session.ProtocolHandler interface for
 // connecting to remote endpoints over SSH. It uses the agent infrastructure
@@ -36,22 +30,9 @@ func (h *protocolHandler) Connect(
 	if err != nil {
 		return nil, err
 	}
-	// Create the endpoint client.
-	return remote.NewEndpointClient(conn, pathInVM(url), session, version, configuration, alpha)
-}
 
-func pathInVM(url *urlpkg.URL) string {
-	p := url.Path
-	if len(p) >= 2 && unicode.IsLetter(rune(p[0])) && string(p[1]) == ":" {
-		// starts with a Windows drive letter, replace with a path separator
-		p = filepath.Join(string(p[0]), p[2:])
-	}
-	elements := []string{mutagenDirInVM}
-	for _, element := range strings.Split(p, string(os.PathSeparator)) {
-		elements = append(elements, element)
-	}
-	// convert to Go/Unix style path
-	return path.Join(elements...)
+	// Create the endpoint client.
+	return remote.NewEndpointClient(conn, filepath.Join("/mutagen", url.Path), session, version, configuration, alpha)
 }
 
 func toHexBigE(v uint32) string {
