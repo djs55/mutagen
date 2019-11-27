443acf739b5042a8ca5503f6779cffa366d306fd
David Scott <dave.scott@docker.com>
2019-11-27 14:22:32 +0000

    Set os to 'all' to make the download URLs less confusing
    Signed-off-by: David Scott <dave.scott@docker.com>

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ rm -rf build
 go run scripts/build.go --mode=release
 ./build/mutagen mutagen legal > build/OSS-LICENSES
 
-${PUTME} --component mutagen --os mac --version "$VERSION" --key "docker-mutagen" --path ./build/mutagen
-${PUTME} --component mutagen --os mac --version "$VERSION" --key "docker-mutagen.exe" --path ./build/cli/windows_amd64
-${PUTME} --component mutagen --os mac --version "$VERSION" --key "mutagen-agent" --path ./build/agent/linux_amd64
-${PUTME} --component mutagen --os mac --version "$VERSION" --key "OSS-LICENSES" --path ./build/OSS-LICENSES
+${PUTME} --component mutagen --os all --version "$VERSION" --key "docker-mutagen" --path ./build/mutagen
+${PUTME} --component mutagen --os all --version "$VERSION" --key "docker-mutagen.exe" --path ./build/cli/windows_amd64
+${PUTME} --component mutagen --os all --version "$VERSION" --key "mutagen-agent" --path ./build/agent/linux_amd64
+${PUTME} --component mutagen --os all --version "$VERSION" --key "OSS-LICENSES" --path ./build/OSS-LICENSES
